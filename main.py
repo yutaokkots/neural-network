271abab6594430668168a57cea630ff53d97afc0
yutaokkots <yuta.okkotsu@gmail.com>
2023-11-03 23:57:27 -0600

    updated and installed django for use as a webserver

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ print("Python: ", sys.version)
 print("Numpy: ", np.__version__)
 print("Matplotlib: ", matplotlib.__version__)
 
-'''
+"""
 Python:  3.10.5 (v3.10.5:f377153967, Jun  6 2022, 12:36:10) [Clang 13.0.0 (clang-1300.0.29.30)]
 Numpy:  1.23.5
 Matplotlib:  3.8.1
-'''
+"""
